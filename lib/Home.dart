e56d41be31ed9e0aa4377b931c2f980f761161c2
MachadoGabriel97 <6249824@aluno.uniasselvi.com.br>
2024-08-12 11:04:50 -0300

    primeiro commit

---
+++ lib/Home.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  TextEditingController controllerTextCep = TextEditingController();
+  Map<String,dynamic> retorno = {};
+
+  void _recuperarCep() async {
+    String cep = controllerTextCep.text.toString();
+    String url = "https://viacep.com.br/ws/${cep}/json/";
+    print(url);
+    http.Response response;
+    response = await http.get(Uri.parse(url));
+    print(response.body.toString());
+    print(response.statusCode.toString());
+    Map<String,dynamic> retornoJson = json.decode(response.body);
+
+    setState(() {
+      retorno = retornoJson ;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+            'Consumo de serviço web Api Cep',
+          style: TextStyle(
+            color: Colors.white
+          ),
+        ),
+        backgroundColor: Colors.blue[300],
+        shadowColor: Colors.black,
+        elevation: 1,
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Column(
+              children: [
+                Image.asset("imagens/icone.jpg"),
+                TextFormField(
+                  controller: controllerTextCep,
+                  decoration: const InputDecoration(
+                    label: Text("Informe o cep"),
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(height: 30,),
+            ElevatedButton(
+                onPressed: _recuperarCep,
+                style: const ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll<Color>(Colors.blue),
+                ),
+                child: const Text(
+                  "Recuperar Cep",
+                  style: TextStyle(color: Colors.white),
+                ),),
+
+            retorno.isEmpty    ? const Text(""):
+            Expanded(
+              child: ListView(
+                children: retorno.entries.map((dados) {
+                  return ListTile(
+                    title: Text(dados.key), // Use 'dados.key' ou qualquer outro dado que faça sentido para o título
+                    subtitle: Text(dados.value), // Assumindo que 'dados.value' é um Map
+                  );
+                }).toList(), // Converte o Iterable em uma lista de widgets
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+
+}
